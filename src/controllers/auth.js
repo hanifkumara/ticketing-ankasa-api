3c1173c96a1dc3ea2a753cd8d8463da63adf3539
hanif <hnflasting@gmail.com>
2021-01-09 00:29:33 +0700

    add forgot password

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs')
 const { v4: uuidv4 } = require('uuid')
 const jwt = require('jsonwebtoken')
 const model = require('../models/index')
+const { sendEmail } = require('../helpers/email')
 
 exports.login = (req, res, next) => {
   const dataBody = req.body
@@ -67,4 +68,54 @@ exports.register = (req, res, next) => {
   .catch(err => {
     return response('error', res, null, 500, {message: 'Internal Server Error'})
   })
+}
+
+exports.forgotPassword = async (req, res, next) => {
+  try {
+    const { email } = req.body
+    const resEmail = await model.users.findOne({ where: { email } })
+    if (!resEmail) {
+      return response('error', res, null, 401, { message: 'Email Not Found' })
+    } else {
+      console.log('ini idnya', resEmail.id)
+      jwt.sign({ myId: resEmail.id }, process.env.SECRET_KEY, { expiresIn: '1d' }, (err, emailToken) => {
+        const url = `${process.env.FRONTEND_URL}/auth/createnewpass/${emailToken}`;
+        sendEmail(email, url)
+        return response('success', res, { token: emailToken, message: 'Send email success. Pelase check your email now' }, 201, null)
+      })
+    }
+  } catch (error) {
+    return response('error', res, null, 500, {message: 'Internal Server Error'})
+  }
+}
+exports.resetPassword = (req, res, next) => {
+  try {
+    const {password} = req.body
+    console.log(password)
+    const authorization = req.headers.authorization
+    if (!authorization) return response('error', res, null, 401, { message: 'You Not Have Token!' })
+    let token = authorization.split(' ')
+    token = token[1]
+    jwt.verify(token, process.env.SECRET_KEY, function (err, decoded) {
+      if (err) {
+        if (err.name === 'JsonWebTokenError') {
+          return response('error', res, null, 401, { message: 'Invalid Token' })
+        } else if (err.name === 'TokenExpiredError') {
+          return response('error', res, null, 401, { message: 'Token Expired' })
+        }
+      }
+      bcrypt.genSalt(10, function (err, salt) {
+        bcrypt.hash(password, salt, function (err, hash) {
+          model.users.update({password: hash}, { where: { id: decoded.myId } })
+            .then(() => {
+              console.log(decoded)
+              return response('success', res, { message: 'Reset Password Success' }, 201, null)
+            })
+        })
+      })
+      console.log(decoded)
+    })
+  } catch (error) {
+    return response('error', res, null, 500, error)
+  }
 }
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { login, register } = require('../controllers/auth')
+const { login, register, forgotPassword, resetPassword } = require('../controllers/auth')
 
 router
   .post('/login', login)
   .post('/register', register)
+  .post('/forgot-password', forgotPassword)
+  .patch('/reset-password', resetPassword)
 
 module.exports = router
