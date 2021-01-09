11dece220edd71950b5a464e50de3057ae84e7b9
DiaryKyosuke <fendianwar36@gmail.com>
2021-01-09 16:43:53 +0700

    verify token on ticket router

--- src/routes/ticket.js
+++ src/routes/ticket.js
@@ -4,10 +4,10 @@ const ticketControllers = require('../controllers/ticketControllers')
 const { verifyToken } = require('../middlewares/verifyToken')
 const { uploadMulter } = require('../middlewares/upload')
 
-router.get('/ticketing', ticketControllers.ticketing)
-router.get('/detail/:id', ticketControllers.detail)
-router.get('/transaction', ticketControllers.transaction)
-router.patch('/update/:id', uploadMulter.single('images'), ticketControllers.update)
-router.post('/create', uploadMulter.single('images'), ticketControllers.create)
+router.get('/ticketing',verifyToken, ticketControllers.ticketing)
+router.get('/detail/:id',verifyToken, ticketControllers.detail)
+router.get('/transaction',verifyToken, ticketControllers.transaction)
+router.patch('/update/:id',verifyToken, uploadMulter.single('images'), ticketControllers.update)
+router.post('/create',verifyToken, uploadMulter.single('images'), ticketControllers.create)
 
 module.exports = router
