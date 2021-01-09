dfae2dfd6cb7651e611695cf5c4ec5a4a5b58ac3
DiaryKyosuke <fendianwar36@gmail.com>
2021-01-09 15:02:18 +0700

    update router booking

--- src/routes/booking.js
+++ src/routes/booking.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const bookingControllers = require('../controllers/bookingControllers')
 const { verifyToken } = require('../middlewares/verifyToken')
 
-router.get('/my-booking/:id', verifyToken, bookingControllers.mybooking)
+router.get('/my-booking', verifyToken, bookingControllers.mybooking)
 router.get('/my-booking-detail/:id', verifyToken, bookingControllers.detail)
 router.post('/create', verifyToken, bookingControllers.create)
 router.patch('/update/:id', verifyToken, bookingControllers.update)
